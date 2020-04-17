6e18932af839a993d4e3e38568dd83af6a8b69fe
Ayvanez <56927592+Ayvanez@users.noreply.github.com>
2020-04-17 21:06:25 +0300

    Add files via upload

---
+++ Лабораторная 1/Упражнение 5/Упражнение 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Упражнение_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the perimetr of equilateral triangle");
+                double P = Convert.ToDouble(Console.ReadLine());
+                double a = P / 3;
+                double p = P / 2;
+                double S = Math.Sqrt(p * (p - a) * (p - a) * (p - a));
+                Console.WriteLine("side\tresult");
+                Console.WriteLine("{0:0.##}\t{1:0.##}", a, S);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("An format exception was thrown: {0}", e.Message);
+            }
+        }
+
+    }
+}
